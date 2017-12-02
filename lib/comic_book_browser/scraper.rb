a4457dd50dec29f5d13b22ebb5990edb79396773
Matija Simin <marcussimon12082015@hotmail.com>
2017-12-02 11:09:54 -0800

    updated the scrape_release_weeks method in Scraper class

--- lib/comic_book_browser/scraper.rb
+++ lib/comic_book_browser/scraper.rb
@@ -2,6 +2,10 @@ class ComicBookBrowser::Scraper
   @@BASE_URL = "https://leagueofcomicgeeks.com"
 
   def self.scrape_release_weeks
+    weeks = []
+    #we get all available release weeks of the comics page to
+    #display to the user
+    @doc = Nokogiri::HTML(open(@@base_url+"/comics"))
     
   end
 end
