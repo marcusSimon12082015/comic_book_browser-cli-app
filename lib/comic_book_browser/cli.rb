702badaf6d2afeaa831b4e890bcc7e076b154bed
Matija Simin <marcussimon12082015@hotmail.com>
2017-12-02 10:42:02 -0800

    Added menu, call and list_weeks methods to CLI class and added scrape_weeks method to Scraper class.

--- lib/comic_book_browser/cli.rb
+++ lib/comic_book_browser/cli.rb
@@ -1,5 +1,17 @@
 class ComicBookBrowser::CLI
   def call
+    list_weeks
+    menu
+  end
 
-  end 
+  def list_weeks
+
+  end
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Select a week to explore or type exit to quit:"
+      input = gets.strip.downcase
+
+  end
 end
--- lib/comic_book_browser/scraper.rb
+++ lib/comic_book_browser/scraper.rb
@@ -1,3 +1,7 @@
 class ComicBookBrowser::Scraper
-  
+  @@BASE_URL = "https://leagueofcomicgeeks.com"
+
+  def self.scrape_release_weeks
+    
+  end
 end
