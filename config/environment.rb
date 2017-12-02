af84b0fae3921d11af7255351b1da1c9ab02941d
Matija Simin <marcussimon12082015@hotmail.com>
2017-12-02 11:28:48 -0800

    Completed the scrape_release_weeks method in Scraper class to return an array of weeks.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,6 @@ require 'nokogiri'
 require 'open-uri'
 
 require_relative '../lib/comic_book_browser/cli'
+
 require_relative '../lib/comic_book_browser/scraper'
+require_relative '../lib/comic_book_browser/week'
--- lib/comic_book_browser/scraper.rb
+++ lib/comic_book_browser/scraper.rb
@@ -7,6 +7,13 @@ class ComicBookBrowser::Scraper
     #display to the user
     @doc = Nokogiri::HTML(open(@@BASE_URL+"/comics"))
     release_dates = @doc.search(".panel-body ul li")
+    release_dates.each_with_index do |release_date, index|
+      release_day = release_date.search('a').text
+      url = release_date.at('a')['href']
+      week = ComicBookBrowser::Week.new(release_day,url)
+      weeks << week
+    end
+    weeks
     binding.pry
   end
 end
