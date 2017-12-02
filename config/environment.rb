2ad354ce0e2e8465f7a477a34d658073e2ee4209
Matija Simin <marcussimon12082015@hotmail.com>
2017-12-02 10:54:15 -0800

    Updated the CLI with goodby method added cli require to environment.rb file.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
+
+require_relative '../lib/comic_book_browser/cli'
--- lib/comic_book_browser/cli.rb
+++ lib/comic_book_browser/cli.rb
@@ -5,13 +5,16 @@ class ComicBookBrowser::CLI
   end
 
   def list_weeks
-
+    puts "BROWSE COMIC BOOK RELEASES:"
   end
   def menu
     input = nil
     while input != "exit"
       puts "Select a week to explore or type exit to quit:"
       input = gets.strip.downcase
-
+    end
+  end
+  def goodbye
+    puts "Bye, see you next Time!"
   end
 end
