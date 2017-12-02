7f110f47f785713ee9ca154c459211bd923021cc
Matija Simin <marcussimon12082015@hotmail.com>
2017-12-02 12:31:25 -0800

    Added a new method display_comic in comicsTableView class.

--- lib/comic_book_browser/comicsTableView.rb
+++ lib/comic_book_browser/comicsTableView.rb
@@ -18,4 +18,30 @@ class ComicBookBrowser::ComicsTableView
       end
     end
   end
-end 
+
+  def display_comic(comic)
+    header title: 'COMIC BOOK DETAILS'.green
+    table border:true do
+      row do
+        column 'TITLE',width:50, color:'red'
+        column comic.title, width:70
+      end
+      row do
+        column 'PUBLISHER', width:50, color:'red'
+        column comic.publisher, width:70
+      end
+      row do
+        column 'PRICE', width:50, color:'red'
+        column comic.price, width:70
+      end
+      row do
+        column 'DESCRIPTION', width:50, color:'red'
+        column comic.description.gsub(/\t/,''), width:70
+      end
+      row do
+        column 'AUTHORS', width:50, color:'red'
+        column comic.authors.join(", "), width:70
+      end
+    end
+  end
+end
